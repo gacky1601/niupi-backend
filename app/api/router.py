41f809be15efb749b25cb28a7ed188ac4b847e82
justYu2001 <jack4781012@gmail.com>
2022-11-18 10:41:21 +0800

    feat: sign up api

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import user
+from . import user, auth
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/user")
+api_router.include_router(auth.router, prefix="/auth")
