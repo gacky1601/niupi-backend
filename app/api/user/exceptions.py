fbf7de243f1239f931dd1965dda296e26b232252
l1ang0226 <ahuhwr886128@gmail.com>
2022-11-22 20:33:30 +0800

    feat: delete user api

--- app/api/user/exceptions.py
+++ app/api/user/exceptions.py
@@ -4,3 +4,8 @@ UserNotFound = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
     detail="User not found"
 )
+
+UserNotExist = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="Cannot delete an account that does not exist"
+)
--- app/api/user/router.py
+++ app/api/user/router.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import Session
 from pydantic import UUID4
 from .schemas import User
 from .service import get_user_by_id
+from .service import delete_user_by_id
 from .dependencies import get_db
 from .exceptions import UserNotFound
+from .exceptions import UserNotExist
 
 router = APIRouter()
 
@@ -18,3 +20,11 @@ def read_user(user_id: UUID4, db: Session = Depends(get_db)):
         raise UserNotFound
 
     return user
+
+
+@router.delete("/{user_id}", status_code=204)
+def delete_user(user_id: UUID4, db: Session = Depends(get_db)):
+    if get_user_by_id(db, user_id) is None:
+        raise UserNotExist
+
+    delete_user_by_id(db, user_id)
