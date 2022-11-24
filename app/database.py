df1231690caaa84735fed0de0cf9dceab32bc056
justYu2001 <jack4781012@gmail.com>
2022-11-24 16:50:44 +0800

    fix: #21

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import config
+from .config import config, Environment
 
 SQLALCHMY_DATABASE_URL = config.POSTGRESS_ADDRESS
 
@@ -12,18 +12,22 @@ Base = declarative_base()
 
 
 def initialize_db():
+    if config.ENV == Environment.DEVELOPMENT:
+        Base.metadata.drop_all(engine)
+
     Base.metadata.create_all(bind=engine)
 
     db = SessionLocal()
 
-    from .api.user.models import Role
+    if config.ENV == Environment.DEVELOPMENT:
+        # initialize role table
+        from .api.user.models import Role
 
-    # initialize role table
-    roles = ["admin", "user"]
-    for index, role in enumerate(roles):
-        new_role = Role(id=index, role=role)
-        db.add(new_role)
+        roles = ["admin", "user"]
+        for index, role in enumerate(roles):
+            new_role = Role(id=index, role=role)
+            db.add(new_role)
 
-    db.commit()
+        db.commit()
 
-    db.close()
+        db.close()
