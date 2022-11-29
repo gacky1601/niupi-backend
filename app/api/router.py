d3dc3a5526ad549cbe935a0333f96dfd4d0eeced
l1ang0226 <ahuhwr886128@gmail.com>
2022-11-29 16:31:01 +0800

    fix: style

--- app/api/router.py
+++ app/api/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+
 from . import user, store, auth, users
 
 api_router = APIRouter()
--- app/api/users/router.py
+++ app/api/users/router.py
@@ -1,9 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from typing import List
-from .service import get_all_user
-from .dependencies import get_db
+
 from app.api.user.schemas import User
+from .dependencies import get_db
+from .service import get_all_user
 
 router = APIRouter()
 
