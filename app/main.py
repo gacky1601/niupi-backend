8a2a0be45c22dbec40c233c002fc285f2282d565
justYu2001 <jack4781012@gmail.com>
2022-12-02 21:59:10 +0800

    feat: hide documations in production

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 
 from . import api
 from .database import initialize_db
+from .config import config, Environment
 
 initialize_db()
 
-app = FastAPI()
+app_configs = {}
+if config.ENV == Environment.PRODUCTION:
+    app_configs["docs_url"] = None
+
+app = FastAPI(**app_configs)
 
 app.include_router(api.router, prefix="/api")
