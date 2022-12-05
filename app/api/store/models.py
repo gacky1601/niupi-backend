bcd4266e3aa1d0966919e34e292693045cc9687e
Yupooooo <gacky1601@gmail.com>
2022-12-05 10:46:56 +0800

    feat: Check constraint implement

--- app/api/store/models.py
+++ app/api/store/models.py
@@ -1,15 +1,17 @@
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 from app.database import Base
+from app.utils.validator import cellphone_number_regex, telephone_number_regex, email_regex
 
 
 class Store(Base):
     __tablename__ = "store"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True,
+                index=True, default=uuid.uuid4)
 
     user_id = Column(
         UUID(as_uuid=True),
@@ -25,3 +27,16 @@ class Store(Base):
     telephone_number = Column(String)
 
     user = relationship("User", back_populates="store", cascade_backrefs=True)
+
+
+CheckConstraint(
+    "REGEXP_LIKE(Store.email," + email_regex,
+    name='emailcheck')
+
+CheckConstraint(
+    "REGEXP_LIKE(Store.cellphone_number," + cellphone_number_regex,
+    name='cellphone_numbercheck')
+
+CheckConstraint(
+    "REGEXP_LIKE(Store.telephone_number," + telephone_number_regex,
+    name='cellphone_numbercheck')
