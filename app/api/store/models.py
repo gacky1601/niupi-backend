d166b76f53cc842ee63cabbff4610d4f7ea86e37
Yupooooo <gacky1601@gmail.com>
2022-12-06 21:45:37 +0800

    style: coding style improve

--- app/api/store/models.py
+++ app/api/store/models.py
@@ -1,17 +1,17 @@
-from sqlalchemy import Column, String, ForeignKey, CheckConstraint
+from sqlalchemy import CheckConstraint, Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 from app.database import Base
-from app.utils.validator import cellphone_number_regex, telephone_number_regex, email_regex
+from app.utils.validator import cellphone_number_regex, email_regex, telephone_number_regex
 
 
 class Store(Base):
     __tablename__ = "store"
 
-    id = Column(UUID(as_uuid=True), primary_key=True,
-                index=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True,
+                default=uuid.uuid4)
 
     user_id = Column(
         UUID(as_uuid=True),
@@ -30,16 +30,16 @@ class Store(Base):
 
 
 CheckConstraint(
-    "REGEXP_LIKE(Store.email," + email_regex,
+    "REGEXP_LIKE(Store.email,{email_regex})",
     name='emailcheck'
 )
 
 CheckConstraint(
-    "REGEXP_LIKE(Store.cellphone_number," + cellphone_number_regex,
+    "REGEXP_LIKE(Store.cellphone_number,{cellphone_number_regex}",
     name='cellphone_numbercheck'
 )
 
 CheckConstraint(
-    "REGEXP_LIKE(Store.telephone_number," + telephone_number_regex,
+    "REGEXP_LIKE(Store.telephone_number,{telephone_number_regex}",
     name='telephone_numbercheck'
 )
