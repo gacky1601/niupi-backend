65da115b8ba27a57b4f38d3d96c69b2ca4666127
justYu2001 <jack4781012@gmail.com>
2022-12-06 10:48:00 +0800

    fix: #49

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from .config import config, Environment
 
 SQLALCHMY_DATABASE_URL = config.POSTGRESS_ADDRESS
 
-engine = create_engine(SQLALCHMY_DATABASE_URL)
+engine = create_engine(SQLALCHMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
