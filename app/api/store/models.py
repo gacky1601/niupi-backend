821ced92e6b5f6a28f9da44a670c4b664d8d7694
Yupooooo <gacky1601@gmail.com>
2022-12-07 09:33:15 +0800

    style: CheckConstraints of store model

--- app/api/store/models.py
+++ app/api/store/models.py
@@ -25,8 +25,7 @@ class Store(Base):
         )
     )
 
-    id = Column(UUID(as_uuid=True), primary_key=True,
-                index=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
 
     user_id = Column(
         UUID(as_uuid=True),
