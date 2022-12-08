089dbdb950c292dc0d3b1dfb9304296f53cd28c9
l1ang0226 <ahuhwr886128@gmail.com>
2022-12-08 14:23:48 +0800

    fix: get item without photo will return none

--- app/api/items/service.py
+++ app/api/items/service.py
@@ -1,13 +1,14 @@
 from sqlalchemy.orm import Session
 from pydantic import UUID4
 
+from .models import Item, ItemPhoto
 
 def get_item_by_item_id(db: Session, item_id: UUID4):
     statement = (
         f"""
         SELECT item.*, array_agg(item_photo.id) as photo_ids
         FROM item
-        JOIN item_photo
+        LEFT JOIN item_photo
         ON item.id=item_photo.item_id
         WHERE item.id='{item_id}'
         GROUP BY item.id
@@ -15,3 +16,5 @@ def get_item_by_item_id(db: Session, item_id: UUID4):
     )
 
     return db.execute(statement).first()
+
+
