943673fcff96a7fea1508fad4df3c23b92e86462
justYu2001 <jack4781012@gmail.com>
2022-12-14 20:39:19 +0800

    style: rename user_id to seller_id in `stores/service.py`

--- app/api/stores/service.py
+++ app/api/stores/service.py
@@ -7,15 +7,15 @@ from . import models
 from .schemas import StoreInitialize
 
 
-def get_store_by_user_id(db: Session, user_id: UUID):
-    return db.query(models.Store).filter(models.Store.user_id == user_id).first()
+def get_store_by_user_id(db: Session, seller_id: UUID):
+    return db.query(models.Store).filter(models.Store.seller_id == seller_id).first()
 
 
-def initialize_store(db: Session, owner_id: UUID4, payload: StoreInitialize):
+def initialize_store(db: Session, seller_id: UUID4, payload: StoreInitialize):
     db.query(models.Store) \
-      .filter(models.Store.user_id == owner_id) \
+      .filter(models.Store.seller_id == seller_id) \
       .update(payload.dict(exclude_none=True), synchronize_session="fetch")
 
-    initialized_store = get_store_by_user_id(db, owner_id)
+    initialized_store = get_store_by_user_id(db, seller_id)
 
     return initialized_store
