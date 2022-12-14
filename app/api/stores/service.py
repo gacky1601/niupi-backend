852f14cb79bba85d4a8ff416776ae346ad38621a
justYu2001 <jack4781012@gmail.com>
2022-12-14 20:49:45 +0800

    style: rename db to database in `store/service.py`

--- app/api/stores/service.py
+++ app/api/stores/service.py
@@ -1,21 +1,21 @@
 from uuid import UUID
 
-from pydantic import UUID4
 from sqlalchemy.orm import Session
+from pydantic import UUID4
 
 from . import models
 from .schemas import StoreInitialize
 
 
-def get_store_by_user_id(db: Session, seller_id: UUID):
-    return db.query(models.Store).filter(models.Store.seller_id == seller_id).first()
+def get_store_by_user_id(database: Session, seller_id: UUID):
+    return database.query(models.Store).filter(models.Store.seller_id == seller_id).first()
 
 
-def initialize_store(db: Session, seller_id: UUID4, payload: StoreInitialize):
-    db.query(models.Store) \
-      .filter(models.Store.seller_id == seller_id) \
-      .update(payload.dict(exclude_none=True), synchronize_session="fetch")
+def initialize_store(database: Session, seller_id: UUID4, payload: StoreInitialize):
+    database.query(models.Store) \
+            .filter(models.Store.seller_id == seller_id) \
+            .update(payload.dict(exclude_none=True), synchronize_session="fetch")
 
-    initialized_store = get_store_by_user_id(db, seller_id)
+    initialized_store = get_store_by_user_id(database, seller_id)
 
     return initialized_store
