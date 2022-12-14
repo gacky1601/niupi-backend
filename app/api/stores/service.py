1bb88bbc80b63ae25a64ee9d10e7a4100c98668e
justYu2001 <jack4781012@gmail.com>
2022-12-14 20:51:37 +0800

    refactor: change UUID4 to UUID in `store/service.py`

--- app/api/stores/service.py
+++ app/api/stores/service.py
@@ -1,7 +1,6 @@
 from uuid import UUID
 
 from sqlalchemy.orm import Session
-from pydantic import UUID4
 
 from . import models
 from .schemas import StoreInitialize
@@ -11,7 +10,7 @@ def get_store_by_user_id(database: Session, seller_id: UUID):
     return database.query(models.Store).filter(models.Store.seller_id == seller_id).first()
 
 
-def initialize_store(database: Session, seller_id: UUID4, payload: StoreInitialize):
+def initialize_store(database: Session, seller_id: UUID, payload: StoreInitialize):
     database.query(models.Store) \
             .filter(models.Store.seller_id == seller_id) \
             .update(payload.dict(exclude_none=True), synchronize_session="fetch")
