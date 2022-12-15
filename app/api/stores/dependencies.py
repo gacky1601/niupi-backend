d1fa61350ce7750a0baec22ab758e4370c139648
justYu2001 <jack4781012@gmail.com>
2022-12-15 11:13:41 +0800

    refactor: seller id vaildation

--- app/api/stores/dependencies.py
+++ app/api/stores/dependencies.py
@@ -1,1 +1,17 @@
+from fastapi import Depends
+from pydantic import UUID4
+from sqlalchemy.orm import Session
+
 from app.api.dependencies import get_db
+from .exceptions import SellerNotFound
+from .models import Store
+from .service import get_store_by_user_id
+
+
+def validate_seller_id(seller_id: UUID4, database: Session = Depends(get_db)) -> Store:
+    store = get_store_by_user_id(database, seller_id)
+
+    if store is None:
+        raise SellerNotFound
+
+    return store
--- app/api/stores/router.py
+++ app/api/stores/router.py
@@ -1,32 +1,23 @@
 from fastapi import APIRouter, Depends
-from pydantic import UUID4
 from sqlalchemy.orm import Session
 
-from . import service
-from .dependencies import get_db
-from .exceptions import SellerNotFound
-from .schemas import Store, StoreUpdate
+from . import models, schemas, service
+from .dependencies import get_db, validate_seller_id
 
 router = APIRouter()
 
 
-@router.get("/{seller_id}", response_model=Store)
-def read_store(seller_id: UUID4, database: Session = Depends(get_db)):
-    store = service.get_store_by_user_id(database, seller_id)
-
-    if store is None:
-        raise SellerNotFound
-
+@router.get("/{seller_id}", response_model=schemas.Store)
+def read_store(store: models.Store = Depends(validate_seller_id)):
     return store
 
 
-@router.patch("/{seller_id}", response_model=Store)
-def update_store(seller_id: UUID4, store: StoreUpdate, database: Session = Depends(get_db)):
-    seller = service.get_store_by_user_id(database, seller_id)
-
-    if seller is None:
-        raise SellerNotFound
-
-    updated_store = service.update_store(database, seller_id, store)
+@router.patch("/{seller_id}", response_model=schemas.Store)
+def update_store(
+    payload: schemas.StoreUpdate,
+    store: models.Store = Depends(validate_seller_id),
+    database: Session = Depends(get_db)
+):
+    updated_store = service.update_store(database, store.seller_id, payload)
 
     return updated_store
