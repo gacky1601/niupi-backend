f9771d7582b67eb55d17356b23f5d96077ac0cd6
justYu2001 <jack4781012@gmail.com>
2022-12-15 09:53:53 +0800

    refactor: change error message of SellerNotFound

--- app/api/stores/exceptions.py
+++ app/api/stores/exceptions.py
@@ -2,5 +2,5 @@ from fastapi import HTTPException, status
 
 SellerNotFound = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
-    detail="There is no store owned by this user"
+    detail="Seller not found"
 )
