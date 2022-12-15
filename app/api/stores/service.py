c9d8a15b1db5b37f006587c81a2f25cdd83811ae
justYu2001 <jack4781012@gmail.com>
2022-12-15 09:33:14 +0800

    style: rename StoreInitialize to StoreUpdate in `stores/service.py`

--- app/api/stores/service.py
+++ app/api/stores/service.py
@@ -3,14 +3,14 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from . import models
-from .schemas import StoreInitialize
+from .schemas import StoreUpdate
 
 
 def get_store_by_user_id(database: Session, seller_id: UUID):
     return database.query(models.Store).filter(models.Store.seller_id == seller_id).first()
 
 
-def update_store(database: Session, seller_id: UUID, payload: StoreInitialize):
+def update_store(database: Session, seller_id: UUID, payload: StoreUpdate):
     database.query(models.Store) \
             .filter(models.Store.seller_id == seller_id) \
             .update(payload.dict(exclude_none=True), synchronize_session="fetch")
