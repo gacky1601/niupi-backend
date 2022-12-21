d909be4277326f3227b50b3af931b6cff6f8df39
justYu2001 <jack4781012@gmail.com>
2022-12-21 18:11:08 +0800

    feat: change store schema

--- app/api/stores/schemas.py
+++ app/api/stores/schemas.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from pydantic import UUID4, BaseModel, EmailStr, constr
 
+from app.utils.address import Address
 from app.utils.validator import cellphone_number_regex, telephone_number_regex
 
 
@@ -9,7 +10,7 @@ class Store(BaseModel):
     id: UUID4
     seller_id: UUID4
     name: Optional[constr(min_length=1, strip_whitespace=True)]
-    address: Optional[constr(min_length=1, strip_whitespace=True)]
+    address: Address
     email: Optional[EmailStr]
     cellphone_number: Optional[constr(min_length=1, strip_whitespace=True)]
     telephone_number: Optional[constr(min_length=1, strip_whitespace=True)]
