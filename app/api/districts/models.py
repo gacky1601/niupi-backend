c0cd57d6a0e8c4c1feca0f4aaae9a654725d493c
justYu2001 <jack4781012@gmail.com>
2022-12-23 11:43:13 +0800

    feat: get all county id api

--- app/api/districts/models.py
+++ app/api/districts/models.py
@@ -1,15 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
 
 from app.database import Base
-
-
-class County(Base):
-    __tablename__ = "county"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    districts = relationship("District")
 
 
 class District(Base):
--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from . import user, stores, auth, users, items, districts
+from . import user, stores, auth, users, items, districts, counties
 
 
 api_router = APIRouter()
@@ -10,4 +10,5 @@ api_router.include_router(users.router, prefix="/users")
 api_router.include_router(stores.router, prefix="/stores")
 api_router.include_router(auth.router, prefix="/auth")
 api_router.include_router(items.router, prefix="/items")
+api_router.include_router(counties.router, prefix="/counties")
 api_router.include_router(districts.router, prefix="/districts")
