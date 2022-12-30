4b5edfc515a79a72971420153608512588828962
l1ang0226 <ahuhwr886128@gmail.com>
2022-12-31 02:56:12 +0800

    test: update item api

--- tests/test_item.py
+++ tests/test_item.py
@@ -103,6 +103,23 @@ def test_update_item(client: TestClient):
     }
     assert response.status_code == 200
 
+    response = client.get(f"/api/items/{item_id}")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "0df1dacb-67f6-495c-b993-49d06a293787",
+        "name": "newitem",
+        "description": "no water",
+        "price": 5000,
+        "store_id": "49b2b69a-512c-4492-a5ea-50633893f8cc",
+        "inventory": 10,
+        "photo_ids": [
+            "c7e7db3b-a097-4fac-81d0-5f999ad33d86",
+            "f5832ea6-4c3c-48f0-8bd6-72ebd8754758"
+        ]
+    }
+
+
 
 def test_update_item_not_exist_item(client: TestClient):
     item_id = "65761879-19ec-45ac-8d3d-41b477bf134b"
